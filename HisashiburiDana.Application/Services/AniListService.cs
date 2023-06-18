283aa5413508bc1c0302fb11fa118a6496beb09d
Abayomi.oyaifo <Abayomi.oyaifo@WEMA-WDB-L9831>
2023-06-18 22:01:20 +0100

    Merging with Master

--- HisashiburiDana.Application/Services/AniListService.cs
+++ HisashiburiDana.Application/Services/AniListService.cs
@@ -97,6 +97,12 @@ namespace HisashiburiDana.Application.Services
 
         }
     
+
+
+
+
+
+
         public async Task<GeneralResponseWrapper<AnimeList>> GetSortedAnimes(Sorter sortBy, int pageNumber)
         {
             _logger.LogInformation($"GetSortedAnimes Request arrived ----- Sort By: {sortBy.ToString()}");
