315dc05175cd8ee123d4316978f36ffe38568d59
Ibrahim.dahiru <Ibrahim.dahiru@WEMA-WDB-L9835>
2023-06-19 11:56:17 +0100

    Added endpoints to delete anime from watchlist,currently watching and already watched

---
+++ HisashiburiDana.Application/Validators/DeleteAnimeValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HisashiburiDana.Contract.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HisashiburiDana.Application.Validators
+{
+    public class DeleteAnimeValidator : AbstractValidator<DeleteAnimeRequest>
+    {
+        public DeleteAnimeValidator()
+        {
+            RuleFor(x => x.AnimeId).NotNull().NotEmpty();
+            RuleFor(x => x.UserId).NotEmpty().NotNull();
+        }
+    }
+}
